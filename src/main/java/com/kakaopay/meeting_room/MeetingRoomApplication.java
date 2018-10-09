b255a7483cef3f6f7e424354435bf98c03a858f1
MINDOCK <parksy8189@gmail.com>
2018-10-09 23:25:52 +0900

    init project structure

---
+++ src/main/java/com/kakaopay/meeting_room/MeetingRoomApplication.java
@@ -0,0 +1,12 @@
+package com.kakaopay.meeting_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingRoomApplication.class, args);
+    }
+}
