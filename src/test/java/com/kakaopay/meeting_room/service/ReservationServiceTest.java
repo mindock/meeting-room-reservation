b577486ea2106e3110a3555ac23b41db4d1bb8e6
MINDOCK <parksy8189@gmail.com>
2018-10-15 03:46:05 +0900

    fix: addReservation 테스트 수정

--- src/test/java/com/kakaopay/meeting_room/service/ReservationServiceTest.java
+++ src/test/java/com/kakaopay/meeting_room/service/ReservationServiceTest.java
@@ -4,6 +4,7 @@ import com.kakaopay.meeting_room.domain.MeetingRoom;
 import com.kakaopay.meeting_room.domain.Reservation;
 import com.kakaopay.meeting_room.dto.ReservationDTO;
 import com.kakaopay.meeting_room.exception.OverlapReservationException;
+import com.kakaopay.meeting_room.repository.MeetingRoomRepository;
 import com.kakaopay.meeting_room.repository.ReservationRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,6 +19,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -28,6 +30,9 @@ public class ReservationServiceTest {
 
     @Mock
     private ReservationRepository reservationRepository;
+
+    @Mock
+    private MeetingRoomRepository meetingRoomRepository;
 
     @InjectMocks
     private ReservationService reservationService;
@@ -84,6 +89,7 @@ public class ReservationServiceTest {
 
     @Test
     public void addReservation_성공() throws ParseException {
+        when(meetingRoomRepository.findById(1L)).thenReturn(Optional.of(meetingRoom));
         when(reservationRepository.findByStartDateAndMeetingRoomId(date, 1L)).thenReturn(reservations);
         when(reservationRepository.findRepeatReservationByDateAndMeetingRoom(date, 1L)).thenReturn(repeatReservations);
 
@@ -100,6 +106,7 @@ public class ReservationServiceTest {
 
     @Test(expected = OverlapReservationException.class)
     public void addReservation_실패() throws ParseException {
+        when(meetingRoomRepository.findById(1L)).thenReturn(Optional.of(meetingRoom));
         when(reservationRepository.findByStartDateAndMeetingRoomId(date, 1L)).thenReturn(reservations);
         when(reservationRepository.findRepeatReservationByDateAndMeetingRoom(date, 1L)).thenReturn(repeatReservations);
 
